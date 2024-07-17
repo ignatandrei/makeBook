6a251587e5a2a7a037b23695c4dda5b227213b06
Andrei Ignat <ignatandrei@yahoo.com>
2024-07-17 07:35:08 +0300

    added github version

---
+++ src/apps/MakeBookSolution/MakeBookLatestVersion/Class1.cs
@@ -0,0 +1,38 @@
+﻿
+namespace MakeBookLatestVersion;
+
+
+[OneOf.GenerateOneOf]
+public partial class ResultsHttp<T> : OneOf.OneOfBase<T,Exception >
+{
+
+}
+
+public class LatestVersion
+{
+
+    public async Task<ResultsHttp<string>> LatestVersionNumber(string owner, string repo)
+    {
+        string url = $"https://api.github.com/repos/{owner}/{repo}/releases/latest";
+
+        using (var client = new HttpClient())
+        {
+            // GitHub API requires a user-agent
+            client.DefaultRequestHeaders.Add("User-Agent", "request");
+            try
+            {
+                var response = await client.GetStringAsync(url);
+                using (JsonDocument doc = JsonDocument.Parse(response))
+                {
+                    var root = doc.RootElement;
+                    var tagName = root.GetProperty("tag_name").GetString();
+                    return tagName;
+                }
+            }
+            catch (Exception e)
+            {
+                return e;
+            }
+        }
+    }
+}
